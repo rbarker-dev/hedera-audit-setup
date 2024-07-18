6cf7ab73385c8320a49f96e6d7bad26ea32f6d8d
Roger Barker <roger.barker@swirldslabs.com>
2024-07-18 01:53:38 -0500

    Generated a top-level bash script and python script to query the repos that need to be audited
    Signed-off-by: Roger Barker <roger.barker@swirldslabs.com>

---
+++ run_audit_prep.sh
@@ -0,0 +1,8 @@
+echo "Calling Python:"
+python3 setup-ci-audit.py -q 2 -i
+echo "Changing permissions on audit_setup.sh"
+chmod 755 audit_setup.sh
+echo "Calling audit_setup.sh (takes a bit)"
+./audit_setup.sh > 2024-Q2-audit_list.txt
+rm audit_setup.sh
+cat 2024-Q2-audit_list.txt
---
+++ setup-ci-audit.py
@@ -0,0 +1,78 @@
+import argparse
+import sys
+from datetime import datetime,timedelta
+
+"""
+gh search repos props.initial-ci-review-date:2024-05-01 --owner=hashgraph --visibility=public --limit 200 --json name --jq '.[].name' | cat
+
+"""
+
+def run_gh_query(date_list:list[str], use_init_date:bool=False):
+    command_template:str = "gh search repos props.last-ci-review-date:[TEMPLATE_DATE] --owner=hashgraph --limit 200 --json name --jq '.[].name' | cat\n"
+    if use_init_date == True:
+        command_template = "gh search repos props.initial-ci-review-date:[TEMPLATE_DATE] --owner=hashgraph --limit 200 --json name --jq '.[].name' | cat\n"
+    with open("audit_setup.sh","w") as shell:
+        for day in date_list:
+            command = command_template
+            shell.write(command.replace("[TEMPLATE_DATE]",day))
+            shell.write("sleep 2\n")
+
+def get_quarter_date_range(quarter:int) -> tuple[str,str]:
+    start:str = ""
+    end:str = ""
+    year:str = datetime.today().strftime("%Y-")
+    if quarter == 1:
+        start = year + "01-01"
+        end = year + "03-31"
+    elif quarter == 2:
+        start = year + "04-01"
+        end = year + "06-30"
+    elif quarter == 3:
+        start = year + "07-01"
+        end = year + "09-30"
+    else:
+        start = year + "10-01"
+        end = year + "12-31"
+    return start, end
+
+    
+
+def get_date_list(quarter:int) -> list[str]:
+    start_date:str = ""
+    end_date:str = ""
+    start_date,end_date = get_quarter_date_range(quarter)
+    init:datetime = datetime.strptime(start_date,"%Y-%m-%d")
+    final:datetime = datetime.strptime(end_date,"%Y-%m-%d")
+
+    delta_days = (final - init).days + 1
+
+    dateList:list[str] = []
+    for x in range(0,delta_days):
+        dateList.append((final - timedelta(days=x)).strftime("%Y-%m-%d"))
+    dateList.reverse()
+    return dateList
+
+def parse_args() -> tuple[str,str,bool]:
+    parser = argparse.ArgumentParser(description="The setup-ci-audit script generates a list of repositories that will be audited for this quarter")
+    parser.add_argument("-q","--quarter",dest="quarter",type=int,metavar="quarter(1,2,3,4)", help="The quarter to start the audit for")
+    parser.add_argument("-i","--use-initial-ci-review-date",dest="use_init",action="store_true",help="Specify whether or not to use initial-ci-review-date custom property")
+
+    args = parser.parse_args()
+
+    if args.quarter < 1 or args.quarter > 4:
+        raise RuntimeError("Quarter must be of 1, 2, 3, or 4")
+    
+    return args.quarter,args.use_init
+
+def main():
+    try:
+        args:tuple[int,bool] = parse_args()
+        dateList:list[str] = get_date_list(quarter=args[0])
+        run_gh_query(date_list=dateList, use_init_date=args[1])
+    except Exception as e:
+        print(e)
+        sys.exit(1) # error code
+
+if __name__ == "__main__":
+    main()
+    sys.exit(0) # clean execution
