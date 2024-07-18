53573859535bded78e1e8766701a3648af3f6f86
Roger Barker <roger.barker@swirldslabs.com>
2024-07-18 01:59:24 -0500

    Updated for Q3 audit list
    Signed-off-by: Roger Barker <roger.barker@swirldslabs.com>

--- run_audit_prep.sh
+++ run_audit_prep.sh
@@ -3,6 +3,6 @@ python3 setup-ci-audit.py -q 2 -i
 echo "Changing permissions on audit_setup.sh"
 chmod 755 audit_setup.sh
 echo "Calling audit_setup.sh (takes a bit)"
-./audit_setup.sh > 2024-Q2-audit_list.txt
+./audit_setup.sh > 2024-Q3-audit_list.txt
 rm audit_setup.sh
-cat 2024-Q2-audit_list.txt
+cat 2024-Q3-audit_list.txt
