d4c831c1be5fd66ef5ed564bc9b233c907db350e
Roger Barker <roger.barker@swirldslabs.com>
2024-07-19 11:45:07 -0500

    Updated audit_issue_script_generator.py to include chmod and handle exceptions
    Signed-off-by: Roger Barker <roger.barker@swirldslabs.com>

--- audit_issue_script_generator.py
+++ audit_issue_script_generator.py
@@ -3,20 +3,41 @@ import sys
 
 def gen_issue_script(audit_list_file:str, quarter:str):
     from datetime import datetime
+    from os import chmod
+
+    print("Generating the Issue Gen shell script")
+
     year:str = datetime.today().strftime("%Y-")
-    title:str = "ci: [" + year + quarter + "] CI/CD Audit Story"
+    title:str = "\"ci: [" + year + quarter + "] CI/CD Audit Story\""
     labels:list[str] = ["github_actions","Audit"]
-    project:str = "DevOps-CI Planning Board"
+    project:str = "\"DevOps-CI Planning Board\""
     audit_template:str = "audit_template.md"
     command:str = "gh issue create --repo hashgraph/[REPO] --project " + project + " --title " + title + " --body-file " + audit_template + " "
     for label in labels:
-        command += "--label " + label + " "
+        command += "--label \"" + label + "\" "
     
-    repo_list:list[str] = []
-    with open(audit_list_file, "r") as audit_list:
-        repo_list = audit_list.readlines()
+    cmd_list:list[str] = []
+
+    try:
+        with open(audit_list_file, "r") as audit_list:
+            for line in audit_list.readlines():
+                repo:str = line.strip()
+                cmd:str = command.replace("[REPO]",repo)
+                cmd_list.append(cmd)
+        
+        audit_script:str = "audit_issue_gen.sh"
+        with open(audit_script, "w") as script:
+            for line in cmd_list:
+                line += "\nsleep 2\n" # need sleep 2 for gh api to be happy
+                script.write(line)
+
+        print("Generation complete. chmod to 755 to enable execution of the script")
     
-    print(repo_list)
+        chmod(path=audit_script,mode=755) # Change mode on the file to ensure executability
+    except Exception as e:
+        raise e
+
+    print("Complete.")
 
 def parse_audit_args() -> tuple[str,str]:
     from os.path import exists as path_exists # just need to check if os.path.exists() returns true for the audit file
@@ -42,6 +63,7 @@ def main():
         audit_list:str = ""
         quarter:str = ""
         audit_list,quarter = parse_audit_args()
+        print("Audit_List file:",audit_list,"Quarter:",quarter)
         gen_issue_script(audit_list_file=audit_list, quarter=quarter)
     except Exception as e:
         print(e)
