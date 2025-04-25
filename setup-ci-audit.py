ee8384702e820cdff0f37d000fcd93740461cb3e
Roger Barker <roger.barker@swirldslabs.com>
2025-04-25 15:16:25 -0500

    chore: Update setup-ci-audit.py and run_audit_prep.sh (#26)
    Signed-off-by: Roger Barker <roger.barker@swirldslabs.com>

--- setup-ci-audit.py
+++ setup-ci-audit.py
@@ -2,16 +2,26 @@ import argparse
 import sys
 from datetime import datetime,timedelta
 
-def run_gh_query(date_list:list[str], orgs:list[str], use_init_date:bool=False):
+def run_gh_query(date_list:list[str], orgs:list[str], use_init_date:bool=False, no_dates_on_repo:bool=False):
     command_template:str = "gh search repos props.last-ci-review-date:[TEMPLATE_DATE] --archived=false --include-forks=false --owner=[ORG_NAME] --limit 1000 --json owner,name --jq '.[] | \"\\(.owner.login),\\(.name)\"' | cat"
     if use_init_date:
         command_template = "gh search repos props.initial-ci-review-date:[TEMPLATE_DATE] --archived=false --include-forks=false --owner=[ORG_NAME] --limit 1000 --json owner,name --jq '.[] | \"\\(.owner.login),\\(.name)\"' | cat"
+
+    # If no_dates_on_repo is True, we will not use the date in the command
+    if no_dates_on_repo:
+        command_template = "gh search repos --archived=false --include-forks=false --owner=[ORG_NAME] --limit 1000 --json owner,name --jq '.[] | \"\\(.owner.login),\\(.name)\"' | cat"
+
     with open("audit_setup.sh","w") as shell:
         for org in orgs:
-            for day in date_list:
-                command = command_template.replace("[TEMPLATE_DATE]",day).replace("[ORG_NAME]",org)
-                shell.write(command + "\n")
-                shell.write("sleep 2\n")
+            if no_dates_on_repo:
+              command = command_template.replace("[ORG_NAME]",org)
+              shell.write(command + "\n")
+              shell.write("sleep 2\n")
+            else:
+              for day in date_list:
+                  command = command_template.replace("[TEMPLATE_DATE]",day).replace("[ORG_NAME]",org)
+                  shell.write(command + "\n")
+                  shell.write("sleep 2\n")
 
 def get_quarter_date_range(quarter:int) -> tuple[str,str]:
     """
@@ -74,18 +84,20 @@ def parse_args() -> tuple[str,str,int,list[str],bool]:
     Parse the command line arguments passed to the script
 
     Returns:
-        tuple[str,str,int,list[str],bool]:
+        tuple[str,str,int,list[str],bool,bool]:
             start (str): The start date for the audit
             end (str): The end date for the audit
             quarter (int): The quarter to start the audit for
             org_names (list[str]): The list of GitHub organization names
             use_init (bool): Specify whether or not to use initial-ci-review-date custom property
+            no_dates_flag (bool): Specify that no dates have been set on the repo for this audit
     """
     parser = argparse.ArgumentParser(description="The setup-ci-audit script generates a list of repositories that will be audited for this quarter")
     parser.add_argument("-q","--quarter",dest="quarter",type=int,metavar="quarter(1,2,3,4)", help="The quarter to start the audit for")
     parser.add_argument("-s","--start",dest="start",type=str,metavar="START (YYYY-MM-DD)", help="Specify a start date for audit prep within a date range")
     parser.add_argument("-e","--end",dest="end",type=str,metavar="END (YYYY-MM-DD)", help="Specify an end date for audit prep within a date range")
     parser.add_argument("-i","--use-initial-ci-review-date",dest="use_init",action="store_true",help="Specify whether or not to use initial-ci-review-date custom property")
+    parser.add_argument("-n","--no-dates-on-repo-flag",dest="no_dates_flag",action="store_true",help="Specify that no dates have been set on the repo for this audit")
     parser.add_argument("-o", "--org", type=str, nargs="+", default=["hashgraph"], dest="org_names", help="GitHub organization name (default: hashgraph)")
 
     args:argparse.Namespace = parser.parse_args()
@@ -105,7 +117,7 @@ def parse_args() -> tuple[str,str,int,list[str],bool]:
 
     start:str = ""
     end:str = ""
-    if not check_quarter:
+    if not check_quarter and not args.no_dates_flag:
         if len(args.start) != 10:
             raise RuntimeError("Start date must be formatted as YYYY-MM-DD")
         else:
@@ -115,7 +127,7 @@ def parse_args() -> tuple[str,str,int,list[str],bool]:
         else:
             end = args.end
     
-    return start, end, quarter, org_names, args.use_init
+    return start, end, quarter, org_names, args.use_init, args.no_dates_flag
 
 def main():
     """
@@ -129,12 +141,14 @@ def main():
     the script will exit with a non-zero status code.
     """
     try:
-        start, end, quarter, org_names, use_init = parse_args()
+        start, end, quarter, org_names, use_init, no_dates_flag = parse_args()
         print("Start date:", start, "\nEnd date:", end, "\nQuarter:", quarter, "\nOrgs:", org_names, "\nUse Initial Date:", use_init)
         use_date_range:bool = quarter not in [1,2,3,4]
-        date_list:list[str] = get_date_list(start=start,end=end,quarter=quarter,use_range=use_date_range)
+        date_list:list[str] = []
+        if not no_dates_flag:
+          date_list = get_date_list(start=start,end=end,quarter=quarter,use_range=use_date_range)
         print("Running GitHub query")
-        run_gh_query(date_list=date_list, orgs=org_names, use_init_date=use_init)
+        run_gh_query(date_list=date_list, orgs=org_names, use_init_date=use_init, no_dates_on_repo=no_dates_flag)
     except Exception as e:
         print(e)
         sys.exit(1) # error code
